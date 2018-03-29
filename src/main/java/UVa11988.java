16835a247fac269dc43d83569522dbc54c2e2a68
Luis F Munoz <luis.munoz@arris.com>
2018-03-29 10:18:58 -0500

    Uva11988 - Broken Keyboard Accepted. Looked up the answer because was getting time limit exception

--- src/main/java/UVa11988.java
+++ src/main/java/UVa11988.java
@@ -1,6 +1,7 @@
 /*
 
-UVa11988 -
+UVa11988 - Broken Keyboard
+
 
 https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=16&page=show_problem&problem=1796
 https://www.udebug.com/UVa/11988
@@ -13,9 +14,12 @@ package uva11988;
 
 // Copy-paste from here...
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -26,6 +30,7 @@ class Main {
         UVa11988 uva = new UVa11988();
         //uva.runTest();
         uva.run();
+        //uva.run2();
     }
 }
 
@@ -34,33 +39,47 @@ class UVa11988 {
 
     private String fileName = "/home/luis/projects/competitive_programming/src/main/resources/uva11988_in.txt";
 
+
+
+    public static void run2 () throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s;
+
+        while ((s=br.readLine())!=null) {
+            LinkedList<Character> value=new LinkedList<>();
+            int pointerLocation=0;
+            for (char c : s.toCharArray()) {
+                if (c=='[') pointerLocation=0;
+                else if (c==']') pointerLocation=value.size();
+                else value.add(pointerLocation++,c);
+            }
+
+            StringBuilder sb=new StringBuilder();
+            for (char c : value) sb.append(c);
+            System.out.println(sb.toString());
+        }
+    }
+
     public void run() {
         //Scanner scan = readFile(fileName);
         Scanner scan =read();
 
-
-
-        LinkedList<Character> result = new LinkedList();
-        ListIterator<Character> iter = result.listIterator(0);
+       LinkedList<Character> result = new LinkedList();
+       ListIterator<Character> iter = result.listIterator(0);
        scan.useDelimiter("");
        while(scan.hasNext()) {
            Character c =  scan.next().charAt(0);
            if (c.equals('[')) {
                iter = result.listIterator(0);
-               continue;
            } else if (c.equals(']')) {
                iter = result.listIterator(result.size());
-               continue;
-           }
-
-           if(c.equals('\n')) {
+           } else if (c.equals('\n')) {
                printList(result);
                result = new LinkedList();
                iter = result.listIterator(0);
-               continue;
+           } else {
+               iter.add(c);
            }
-
-           iter.add(c);
        }
 
 
@@ -90,10 +109,16 @@ class UVa11988 {
 
     private void printList(LinkedList<Character> linkedList) {
 
+        StringBuilder sb=new StringBuilder();
+        for (char c : linkedList) sb.append(c);
+        System.out.println(sb.toString());
+
+        /*
         for (Character x : linkedList) {
             System.out.print(x);
         }
         System.out.println();
+        */
     }
 
     ////////////////////////////////////////////////////////////////////
